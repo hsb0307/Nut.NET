183b3fa4819886c5a89a9a19eb3872629195f8df
laixiaotian <lanfengqi@126.com>
2016-01-07 13:34:03 +0800

    LanguageSelector

---
+++ src/Presentation/Nut.Web/Administration/Controllers/CommonController.cs
@@ -0,0 +1,59 @@
+﻿using Nut.Admin.Extensions;
+using Nut.Admin.Models.Common;
+using Nut.Core;
+using Nut.Services.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nut.Admin.Controllers
+{
+    public class CommonController : BaseAdminController
+    {
+        #region Fields
+        private readonly ILanguageService _languageService;
+        private readonly IWorkContext _workContext;
+        private readonly IStoreContext _storeContext;
+        #endregion
+
+        #region Ctor
+        public CommonController(ILanguageService languageService,
+            IWorkContext workContext,
+            IStoreContext storeContext) {
+            this._languageService = languageService;
+            this._storeContext = storeContext;
+            this._workContext = workContext;
+        }
+        #endregion
+
+
+
+        [ChildActionOnly]
+        public ActionResult LanguageSelector() {
+            var model = new LanguageSelectorModel();
+            model.CurrentLanguage = _workContext.WorkingLanguage.ToModel();
+            model.AvailableLanguages = _languageService
+                .GetAllLanguages(storeId: _storeContext.CurrentStore.Id)
+                .Select(x => x.ToModel())
+                .ToList();
+            return PartialView(model);
+        }
+
+        public ActionResult SetLanguage(int langid, string returnUrl = "") {
+            var language = _languageService.GetLanguageById(langid);
+            if (language != null) {
+                _workContext.WorkingLanguage = language;
+            }
+
+            //home page
+            if (String.IsNullOrEmpty(returnUrl))
+                returnUrl = Url.Action("Index", "Home", new { area = "Admin" });
+            //prevent open redirection attack
+            if (!Url.IsLocalUrl(returnUrl))
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
+            return Redirect(returnUrl);
+        }
+    }
+}
---
+++ src/Presentation/Nut.Web/Administration/Models/Common/LanguageSelectorModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Nut.Admin.Models.Localization;
+using Nut.Web.Framework.Mvc;
+
+namespace Nut.Admin.Models.Common
+{
+    public partial class LanguageSelectorModel : BaseNutModel {
+        public LanguageSelectorModel()
+        {
+            AvailableLanguages = new List<LanguageModel>();
+        }
+
+        public IList<LanguageModel> AvailableLanguages { get; set; }
+
+        public LanguageModel CurrentLanguage { get; set; }
+    }
+}
